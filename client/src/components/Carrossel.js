368952375b8257412e17308df2ebfe3596db272f
Wagner Lustosa <git@github.com:CoDigo93/ALUNOS.git>
2020-11-18 02:48:51 -0300

    construção de layout carrossel[MOBILE][WEB]

--- client/src/components/Carrossel.js
+++ client/src/components/Carrossel.js
@@ -1,12 +1,60 @@
 import React from 'react'
 import produto from '../assets/prod02.svg'
+import thumb from '../assets/thumb.svg'
 
 const Carrossel = () =>{
     return(
-        <div className='product'>
-             
-            <img src ={produto} alt='produto' />
+        <>
+        <div className='carrossel'>
+           <a href='/#'>
+            <video className='thumb' src ={thumb}  alt='produto' />
+            <video className='big' src ={produto} alt='produto' />
+            <input id='carrossel__radio1' type='radio' name='radio'/>
+            
+          </a> 
+          <a href='/#'>
+            <img className='thumb' src ={thumb}  alt='produto' />
+            <img className='big' src ={produto} alt='produto' />
+            <input id='carrossel__radio2' type='radio' name='radio'/>
+              
+          </a>
+          
+          <a href='/#'>
+            <img className='thumb' src ={thumb}  alt='produto' />
+            <img className='big' src ={produto} alt='produto' />
+            <input id='carrossel__radio3' type='radio' name='radio'/>
+            
+          </a>
+          
+          <a href='/#'>
+            <img className='thumb' src ={thumb}  alt='produto' />
+            <img className='big' src ={produto} alt='produto' />
+            <input id='carrossel__radio4' type='radio' name='radio'/>
+            
+          </a>
+          
+          <a href='/#'>
+            <img className='thumb' src ={thumb}  alt='produto' />
+            <img className='big' src ={produto} alt='produto' />
+            <input id='carrossel__radio5' type='radio' name='radio'/>
+            
+          </a>
+          
+          <a href='/#'>
+            
+            <img className='big__featured' src ={produto} alt='produto' />
+          </a>
+
+          <div className='carrossel__manual-btn'>
+            <label for='carrossel__radio1' className='manual-btn'/>
+            <label for='carrossel__radio2' className='manual-btn'/>
+            <label for='carrossel__radio3' className='manual-btn'/>
+            <label for='carrossel__radio4' className='manual-btn'/>
+            <label for='carrossel__radio5' className='manual-btn'/>
         </div>
+        </div>
+        
+        </>
         )
 }
 
