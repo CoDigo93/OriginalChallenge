0f4aa84d893d968ba0953dacf53d0edc1ee8f63a
Wagner Lustosa <git@github.com:CoDigo93/ALUNOS.git>
2020-12-01 01:01:06 -0300

    finalização do layout modal de confirmação de compra[WEB][MOBILE]/iniciando carrinho de compra

---
+++ client/src/components/ShoppingCart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+    return (
+        <div className='shoppingCart'>
+            
+
+        </div>
+    )
+}
+
+export default ShoppingCart;
